58dcb831cbb3c895fee4c6637cfb4e8c470b42d1
belarminon <belarnicolau@hotmail.com>
2026-04-08 21:08:08 -0300

    Ajustes no go.yml e db.go

--- database/db.go
+++ database/db.go
@@ -1,8 +1,11 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strings"
+
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,16 +16,31 @@ var (
 	err error
 )
 
+func envOrDefault(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok && value != "" {
+		return value
+	}
+	if value, ok := os.LookupEnv(strings.ToLower(key)); ok && value != "" {
+		return value
+	}
+	return fallback
+}
+
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=" + os.Getenv("HOST") + 
-					" user=" + os.Getenv("USER") + 
-					" password=" + os.Getenv("PASSWORD") + 
-					" dbname=" + os.Getenv("DBNAME") + 
-					" port=" + os.Getenv("PORT") + 
-					" sslmode=disable"
+	host := envOrDefault("HOST", "localhost")
+	user := envOrDefault("USER", "root")
+	password := envOrDefault("PASSWORD", "root")
+	dbname := envOrDefault("DBNAME", "root")
+	port := envOrDefault("PORT", "5432")
+
+	stringDeConexao := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port,
+	)
+
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
+		log.Panicf("Erro ao conectar com banco de dados: %v", err)
 	}
 
 	DB.AutoMigrate(&models.Aluno{})
